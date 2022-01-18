f509f9de2917906f0395f8a60ce4795514ab5745
nugobyte <leadertfc@gmail.com>
2022-01-18 01:45:40 -0500

    miscellaneous

---
+++ misc/Power_Checker.cs
@@ -0,0 +1,31 @@
+using Pwr;
+using System;
+using static System.Console;
+
+class Pwr
+{
+  static void Main()
+  {
+    int bs,max;
+    float i,j,temp; 
+    bool flag = new bool();
+    {
+      Write("Enter base number to identify the powers associated with it: ");
+        bs = int.Parse(ReadLine());
+      Write("Enter the max for the range of numbers starting from zero: ");
+        max = int.Parse(ReadLine());
+    }
+
+    for (i = 0; i <= max; i++)
+    {
+      temp = i;
+      for (j = 0; j < i; j++)
+      {
+        temp /= bs;
+        flag = (temp == 1.0f) ? true : false;
+        if (flag == true) break;
+      }
+      Write( (flag) ? ($"{i} is a power of {bs}.\n") : String.Empty);
+    }
+  }
+}
